c2d8b9fd322dda46d0bc465afc102e315fe5e807
Balpreet Singh <singhbalpreet007@gmail.com>
2023-07-09 18:29:45 -0400

    Fix darkening effect in script.js

--- js/script.js
+++ js/script.js
@@ -1,59 +1,51 @@
 const body = document.querySelector("body");
 const container = document.querySelector(".container")
-
-// const n = prompt("n?");
-// const width = `${1/n * 100}%`;
-// console.log(width);
-// body.style.setProperty("--flex-basis", width)
-
-// console.log(item);
-// item.style.flexBasis = width;
-
-const n = 2;
-const width = `${1/n * 100}%`;
-body.style.setProperty("--flex-basis", width)
-
-
-for (let i = 0; i < n * n; i++) {
-    const newItem = document.createElement("div");
-    newItem.classList.add("item");
-    container.appendChild(newItem);
-}
-
-const item = document.querySelector(".item")
-const items = document.querySelectorAll(".item")
-
-function makeBlack() {
-    console.log("Hello");
-    e.style.backgroundColor = "yellow";
-}
-
 const originalRGB = [0, 255, 255]
 
-const RGB1 = 0;
-const RGB2 = 255;
-const RGB3 = 255;
+function createGrid(n) {
+    const width = `${1/n * 100}%`;
+    body.style.setProperty("--flex-basis", width);
+    
+    for (let i = 0; i < n * n; i++) {
+        const newItem = document.createElement("div");
+        newItem.classList.add("item");
+        container.appendChild(newItem);
+    }
+}
 
-items.forEach((item) => {
-    item.addEventListener("mouseover", () => {
-        console.log("Hello");
-        item.style.backgroundColor = getRGBString(originalRGB);
-        console.log(item.style.backgroundColor);
-        let newRGB = [200, 200, 200];
-        newRGB.forEach((value, index) => {
-            newRGB[index] = darkenBy10Percent(originalRGB[1], value);
-            console.log(value);
+function main() {
+    const n = 16;
+    createGrid(16);
+    const items = document.querySelectorAll(".item");
+
+    items.forEach((item) => {
+        item.addEventListener("mouseover", (e) => {
+    
+            if (item.style.backgroundColor === "") {
+                item.style.backgroundColor = getRGBString(originalRGB);
+                console.log(item.style.backgroundColor);
+                item.classList.add("10");
+                return;
+            }
+            const percent = item.className.split(" ").pop();
+            let newRGB = [];
+            
+            originalRGB.forEach((rgbValue) => {
+                newRGB.push(getDarkerRGB(rgbValue, percent))
+            });
+            
+            item.style.backgroundColor = getRGBString(newRGB);
+            item.classList.replace(percent, +percent + 10);
         });
-        // console.log(darkenBy10Percent(originalRGB[1], newRGB[1]));
-        console.log(newRGB);
-        item.style.backgroundColor = getRGBString(newRGB);
-    });
-})
+    })
+}
 
 function getRGBString(rgbArray) {
     return `rgb(${rgbArray[0]},${rgbArray[1]},${rgbArray[2]})`;
 }
 
-function darkenBy10Percent(originalRGB, newRGB) {
-    return newRGB - (originalRGB * 0.1);
+function getDarkerRGB(rgbToChange, percent) {
+    return rgbToChange * (1 - percent/100); 
 }
+
+main()
